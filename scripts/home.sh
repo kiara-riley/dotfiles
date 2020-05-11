25ec513c0adedc4f80dca724390687e8cf3ab0fa
Kraig McKernan <kraigmckernan@gmail.com>
2020-05-11 14:13:55 -0600

    I dunno why this needs to be added but shrug

--- scripts/home.sh
+++ scripts/home.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 xrandr --output eDP-1 --off --output DP-2 --primary --mode 3840x1600 --pos 0x0 --rotate normal
+xrandr --auto
+xrandr --output eDP-1 --off --output DP-2 --primary --mode 3840x1600 --pos 0x0 --rotate normal
