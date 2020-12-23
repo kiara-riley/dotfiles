69e3007c51c5f88b2a129e7fe029e4c813c7063c
Kraig McKernan <kraigmckernan@gmail.com>
2020-12-23 10:02:04 -0700

    Timeout screen

--- scripts/home.sh
+++ scripts/home.sh
@@ -2,3 +2,5 @@
 xrandr --output eDP-1 --off --output DP-2 --primary --mode 3840x1600 --pos 0x0 --rotate normal
 xrandr --auto
 xrandr --output eDP-1 --off --output DP-2 --primary --mode 3840x1600 --pos 0x0 --rotate normal
+xset -dpms
+xset s 3600 3600
