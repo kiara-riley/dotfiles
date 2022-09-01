a08e52d2dd54f3cb795a867300862d4de95996bc
Kiara McKernan <kiara.riley.mck@gmail.com>
2022-09-01 11:55:16 -0700

    Fix agent in tmux and add reshell for nix

--- bash/alias.sh
+++ bash/alias.sh
@@ -7,3 +7,16 @@ alias cc="xclip -selection c"
 alias refeh="feh --randomize --bg-fill ~/Pictures/wallpapers/"
 
 alias vim="nvim" # I've used this for a few years and I like it enough as a total replacement
+
+reshell() {
+  [[ ! -z "$IN_NIX_SHELL" ]] && { exit 1; return; }
+  nix-shell || reshell
+}
+
+newagent() {
+  eval $(ssh-agent)
+
+  if test "$SSH_AUTH_SOCK" ; then
+    ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+  fi
+}
