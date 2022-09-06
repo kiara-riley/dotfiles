b5e24421681defc3be801d7edb022f4abcd1937b
Kiara McKernan <kiara.riley.mck@gmail.com>
2022-09-06 12:38:37 -0700

    Connect to system managed gpg agent automatically

--- bash/general.sh
+++ bash/general.sh
@@ -23,3 +23,5 @@ export HISTCONTROL=ignoreboth:erasedups
 export SSH_AUTH_SOCK=$HOME/.ssh/ssh_auth_sock
 
 export FZF_DEFAULT_OPTS="--bind 'ctrl-space:clear-query' --info inline"
+
+gpg-connect-agent -q /bye
