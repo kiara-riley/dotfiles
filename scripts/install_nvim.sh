c5486e0b257233999df167010486c8efd3f5c9c4
Kiara McKernan <kiara.riley.mck@gmail.com>
2023-09-28 10:05:50 -0700

    Small changes for updated fedora

--- scripts/install_nvim.sh
+++ scripts/install_nvim.sh
@@ -1,5 +1,5 @@
 if [[ -f "/etc/fedora-release" ]];
 then
   sudo dnf install -y neovim the_silver_searcher
-  pip3 install --user --upgrade pynvim
+  #pip3 install --user --upgrade pynvim
 fi
--- scripts/install_x.sh
+++ scripts/install_x.sh
@@ -1,9 +1,10 @@
+set -euo pipefail
 
 if [[ -f "/etc/fedora-release" ]];
 then
-  sudo dnf install -y tilix NetworkManager-tui rofi arandr pavucontrol feh compton xclip network-manager-applet polybar
-  sudo dnf copr enable -y gregw/i3desktop
+  # TODO Migrate compton to picom
+  sudo dnf install -y tilix NetworkManager-tui rofi arandr pavucontrol feh xclip network-manager-applet polybar
   sudo dnf install -y i3-gaps i3status i3lock acpi
-  sudo dnf copr enable -y vishalvvr/fontawesome-fonts # Normal repo out of date
+  sudo dnf copr enable -y vishalvvr/fontawesome-fonts # Normal repo out of date, check again @ fedora 39
   sudo dnf install -y fontawesome-free-fonts
 fi
