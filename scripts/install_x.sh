1dfe49733c311922bbccf8c72d423dfcc00a4919
Kiara McKernan <kiara.riley.mck@gmail.com>
2023-10-17 13:34:31 -0700

    Switch to picom

--- scripts/install_x.sh
+++ scripts/install_x.sh
@@ -2,8 +2,8 @@ set -euo pipefail
 
 if [[ -f "/etc/fedora-release" ]];
 then
-  # TODO Migrate compton to picom
   sudo dnf install -y tilix NetworkManager-tui rofi arandr pavucontrol feh xclip network-manager-applet polybar
+  sudo dnf install -y picom
   sudo dnf install -y i3-gaps i3status i3lock acpi
   sudo dnf copr enable -y vishalvvr/fontawesome-fonts # Normal repo out of date, check again @ fedora 39
   sudo dnf install -y fontawesome-free-fonts
